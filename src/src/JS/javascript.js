69b4a849ed969a87e2979f986484192fe9f10d53
Petar Zagorchev <petar.zagorcheff@gmail.com>
2024-11-16 22:36:20 +0000

    Update javascript.js file
    - Add function Input_Decimal().
    - Add function Calculator_Reset().

--- src/src/JS/javascript.js
+++ src/src/JS/javascript.js
@@ -1,3 +1,4 @@
+// Creates an object to keep track of values
 const Calculator = {
     Display_Value: '0',
     First_Operand: null,
@@ -5,6 +6,7 @@ const Calculator = {
     operator: null,
 };
 
+//This function modifies values each time a button is clicked
 function Input_Digit(digit) {
     const { Display_Value, Wait_Second_Operand } = Calculator;
     if (Wait_Second_Operand === true) {
@@ -13,4 +15,21 @@ function Input_Digit(digit) {
     } else {
         Calculator.Display_Value = Display_Value === '0' ? digit : Display_Value + digit;
     }
+}
+
+//This function handles decimal points
+function Input_Decimal(dot) {
+    
+    if (Calculator.Wait_Second_Operand === true) return;
+    if (!Calculator.Display_Value.includes(dot)) {
+        Calculator.Display_Value += dot;
+    }
+}
+
+//This function reset to initiated state
+function Calculator_Reset() {
+    Calculator.Display_Value = '0';
+    Calculator.First_Operand = null;
+    Calculator.Wait_Second_Operand = false;
+    Calculator.operator = null;
 }
